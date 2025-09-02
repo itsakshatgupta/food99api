46975e9fce742b64fc4b5e49f8ad353a9fdb7e03
Akshat Gupta <akshatguptanov@gmail.com>
2025-09-03 01:28:42 +0530

    Add timer in cart

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
-DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = os.getenv("DEBUG", "False") == "True"D
+DEBUG=True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 INSTALLED_APPS = [
