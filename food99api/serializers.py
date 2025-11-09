f4e2deb2dcca2e8c0166e88fb3545a25aff20d41
Akshat Gupta <akshatguptanov@gmail.com>
2025-11-10 03:10:25 +0530

    conversition

--- food99api/serializers.py
+++ food99api/serializers.py
@@ -26,6 +26,16 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         }
         return data
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'user_type', 'phone')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
 
 # -------------------------
 # USER SERIALIZER
--- food99api/views.py
+++ food99api/views.py
@@ -7,7 +7,14 @@ from .serializers import (
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from .models import CustomUser
 
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    
 # -------------------------
 # USER CRUD
 # -------------------------
