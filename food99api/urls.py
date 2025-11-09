f77aabb3f9b29d2866888944319c3f73a862659c
Akshat Gupta <akshatguptanov@gmail.com>
2025-11-10 03:57:30 +0530

    conversition

--- food99api/urls.py
+++ food99api/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView
 )
 from .views import (
-    HomeSectionsView, RegisterView, UserViewSet, BuyerProfileViewSet, SellerViewSet, ProductViewSet, MessageViewSet, LeadViewSet
+    HomeSectionsView, RegisterView, UserViewSet, BuyerProfileViewSet, SellerViewSet, ProductViewSet, MessageViewSet, LeadViewSet, CustomTokenObtainPairView
 )
 
 router = DefaultRouter()
@@ -18,7 +18,7 @@ router.register(r'leads', LeadViewSet)
 urlpatterns = [
     path('home-sections/', HomeSectionsView.as_view(), name='home-sections'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
