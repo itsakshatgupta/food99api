b6dd6f99a159a5b297a479268e6017d152b6e859
Akshat Gupta <akshatguptanov@gmail.com>
2026-02-27 02:06:32 +0530

    otp version

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 # DEBUG = os.getenv("DEBUG", "False") == "True"D
 DEBUG=True
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 INSTALLED_APPS = [
     # "daphne",   # ASGI server
